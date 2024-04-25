36050ff191d65eb3d7d36ab5cb1fe0c2219ea3f7
APramod2002 <annampramodh@gmail.com>
2024-04-25 02:14:46 -0500

    assignment 8 commit

--- assignment8/BaseballGame.js
+++ assignment8/BaseballGame.js
@@ -1,3 +1,37 @@
 class BaseballGame {
-
+    constructor() {
+        this.secretKey = [];
+        this.yourGuess = [];
+        this.newSecretKey();
+    }
+    
+    newSecretKey() {
+        this.secretKey = []; 
+        while(this.secretKey.length < 3) {
+            let num = Math.floor(Math.random() * 10);
+            if (!this.secretKey.includes(num)) {
+                this.secretKey.push(num);
+            }
+        }
+    }
+    
+    makeGuess(a) {
+        this.yourGuess = a.split('').map(Number);
+        return this.checkGuess();
+    }
+    
+    checkGuess() {
+        let strikes = 0;
+        let balls = 0;
+        
+        for (let i = 0; i < this.yourGuess.length; i++) {
+            if (this.yourGuess[i] === this.secretKey[i]) {
+                strikes++;
+            } else if (this.secretKey.includes(this.yourGuess[i])) {
+                balls++;
+            }
+        }
+        
+        return { strikes, balls };
+    }
 }
--- assignment8/assignment8.js
+++ assignment8/assignment8.js
@@ -1,1 +1,51 @@
 const game = new BaseballGame();
+let key = document.getElementById('key');
+let guess = document.getElementById('guess');
+let newbtn = document.getElementById('new');
+let digitButtons = document.querySelectorAll('.digit');
+let table = document.getElementById('tbody-stat');
+
+function updateDisplay() {
+    guess.textContent = game.yourGuess.join(', ');
+}
+
+function resetGame() {
+    game.newSecretKey();
+    key.textContent = game.secretKey.join(', ');
+    game.yourGuess = [];
+    updateDisplay();
+    table.innerHTML = '';
+}
+
+newbtn.addEventListener('click', resetGame);
+
+digitButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        if (game.yourGuess.length < 3) {
+            game.yourGuess.push(parseInt(this.textContent));
+            updateDisplay();
+            
+            if (game.yourGuess.length === 3) {
+                let result = game.makeGuess(game.yourGuess.join(''));
+                
+                let row = document.createElement('tr');
+
+                row.innerHTML = `<td>${game.yourGuess.join(', ')}</td>
+                                      <td>${result.balls}</td>
+                                      <td>${result.strikes}</td>`;
+                table.appendChild(row);
+                
+                if (result.strikes === 3) {
+                    alert(`Strike Out---\nThe key was ${game.secretKey}\n<New> to play again.`);
+                    updateDisplay();
+                } else {
+                    game.yourGuess = []; 
+                    updateDisplay();
+                }
+            }
+        }
+    });
+});
+
+
+resetGame();
